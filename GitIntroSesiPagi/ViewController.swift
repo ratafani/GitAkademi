53d62f2581c2cdf6fe5fc6b336d1c3950aecf5b1
Tafani Rabbani <ratafani@gmail.com>
2021-06-23 18:31:27 +0700

    auto layout

--- GitIntroSesiPagi/ViewController.swift
+++ GitIntroSesiPagi/ViewController.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-   
-    var viewlist = ["tafa","taftaf","fani","bani","raba","tafani"]
+    
+    var names = ["tafa","taftaf","fani","bani","raba","tafani"]
     var umurWorld  : Int?
     
     @IBOutlet weak var pageController: UIPageControl!
@@ -19,8 +19,9 @@ class ViewController: UIViewController {
         
         collectionView.dataSource = self
         collectionView.delegate = self
-
-        pageController.numberOfPages = viewlist.count
+        
+        pageController.numberOfPages = names.count
+        
         
     }
     override func viewWillAppear(_ animated: Bool) {
@@ -32,17 +33,16 @@ class ViewController: UIViewController {
 
 extension ViewController : UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        viewlist.count
+        names.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? CustomCollectionViewCell else { return UICollectionViewCell() }
-        cell.loadData(item: viewlist[indexPath.row])
+        cell.loadData(item: names[indexPath.row])
         cell.backgroundColor = UIColor.init(red: CGFloat.random(in: 0.5...1), green: CGFloat.random(in: 0.5...1), blue: CGFloat.random(in: 0.5...1), alpha: 1)
         cell.layer.cornerRadius = 10
         return cell
     }
-    
 }
 
 extension ViewController : UICollectionViewDelegate,UICollectionViewDelegateFlowLayout{
@@ -53,18 +53,20 @@ extension ViewController : UICollectionViewDelegate,UICollectionViewDelegateFlow
         let currentPage = Int(ceil(x/w))
         self.pageController.currentPage = currentPage
     }
+    
     //size cell
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        var itemWidth = collectionView.frame.size.width
+        let itemWidth = collectionView.frame.size.width
         let itemHeight = (collectionView.frame.size.height)
         return CGSize(width: itemWidth, height: itemHeight)
     }
-// spacing
+    // spacing
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 0
     }
-//
+    //
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 0
     }
 }
+
