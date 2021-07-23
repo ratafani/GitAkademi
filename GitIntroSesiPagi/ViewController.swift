e8b396bc036616c3e24567b69e0df2bc5d7c4f42
Tafani Rabbani <ratafani@gmail.com>
2021-07-23 08:15:29 +0700

    addiing barcode observation

--- GitIntroSesiPagi/ViewController.swift
+++ GitIntroSesiPagi/ViewController.swift
@@ -15,31 +15,37 @@ class ViewController: UIViewController {
         let label = UILabel()
         label.numberOfLines = 0
         label.textAlignment = .center
+        label.backgroundColor = .red
         return label
     }()
     
     private let image : UIImageView = {
         let image = UIImageView()
-        image.image = UIImage(named: "example3")
+        image.image = UIImage(named: "barcodeExample")
         image.contentMode = .scaleAspectFill
         return image
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(label)
         view.addSubview(image)
+        view.addSubview(label)
         
+        label.text = ""
         textRecognition(image: image.image ?? UIImage())
     }
 
     override func viewDidLayoutSubviews() {
         image.frame = CGRect(x: 20, y: view.safeAreaInsets.top, width: view.frame.size.width-20, height: view.frame.size.height/4)
         
-        label.frame = CGRect(x: 20, y: view.center.y, width: view.frame.size.width*0.8, height: view.frame.size.height*0.8)
+        label.frame = CGRect(x: view.center.x, y: view.center.y, width: view.frame.size.width*0.8, height: view.frame.size.height*0.5)
+        label.center = view.center
     }
     
     func textRecognition(image : UIImage){
+        
+        
+        
         //prepare CGImage
         guard let cgImage = image.cgImage else {return}
         
@@ -54,13 +60,25 @@ class ViewController: UIViewController {
             let list = observation.compactMap{$0.topCandidates(1).first?.string}.joined(separator: ",")
             
             DispatchQueue.main.async {
-                self.label.text = list
+                self.label.text! += "text : " + list
+            }
+        }
+        
+        let req2 = VNDetectBarcodesRequest { request, error in
+            guard let observation = request.results as? [VNBarcodeObservation]
+                  ,error == nil else{return}
+            
+            let list = observation.compactMap{$0.payloadStringValue}
+                .joined(separator: ",")
+            
+            DispatchQueue.main.async {
+                self.label.text! += "barcode : " + list
             }
         }
         
         //handler perform the request
         do{
-            try handler.perform([req])
+            try handler.perform([req2,req])
         }catch{
             print(error)
         }
