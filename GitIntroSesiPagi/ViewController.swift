3949ed3a912d80bc87529989ed870ee042b31bcb
Tafani Rabbani <ratafani@gmail.com>
2021-10-28 14:38:35 +0700

    cloudkit with combine

--- GitIntroSesiPagi/ViewController.swift
+++ GitIntroSesiPagi/ViewController.swift
@@ -6,13 +6,14 @@
 //
 
 import UIKit
-
+import Combine
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var myTableView: UITableView!
     
     var items = [String]()
+    private var cancelable = Set<AnyCancellable>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,15 +25,15 @@ class ViewController: UIViewController {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
         
-        CloudkitManager.shared.fetchItem(recordType: "MyItem", onSuccess: {
-            [weak self] items in
-            let newItems = items.map{$0 as! String}
-            DispatchQueue.main.async {
-                self?.items = newItems
+        CloudkitManager.shared.fetchItem(recordType: "MyItem")
+        
+        CloudkitManager.shared.$myItems
+            .receive(on: RunLoop.main)
+            .sink { [weak self] items in
+                self?.items = items
                 self?.myTableView.reloadData()
             }
-            
-        })
+            .store(in: &cancelable)
     }
     
     @objc func didTapAdd(){
@@ -46,16 +47,7 @@ class ViewController: UIViewController {
         
         alert.addAction(UIAlertAction(title: "Add", style: .default, handler : { _ in
             if let field = alert.textFields?.first,let text = field.text, !text.isEmpty{
-                CloudkitManager.shared.saveNameItem(name: text){
-                    CloudkitManager.shared.fetchItem(recordType: "MyItem", onSuccess: {
-                        [weak self] items in
-                        let newItems = items.map{$0 as! String}
-                        DispatchQueue.main.async {
-                            self?.items = newItems
-                            self?.myTableView.reloadData()
-                        }
-                    })
-                }
+                CloudkitManager.shared.saveNameItem(name: text)
             }
         }))
         
@@ -67,78 +59,6 @@ class ViewController: UIViewController {
 }
 
 
-import CloudKit
-
-
-
-class CloudkitManager{
-    
-    enum DBType {
-        case dbPublic
-        case dbPrivate
-        case dbShared
-    }
-    
-    private let dbPublic = CKContainer(identifier: "iCloud.GitAkademi.exercise").publicCloudDatabase
-    private let dbShared = CKContainer(identifier: "iCloud.GitAkademi.exercise").sharedCloudDatabase
-    private let dbPrivate = CKContainer(identifier: "iCloud.GitAkademi.exercise").privateCloudDatabase
-    
-    static let shared = CloudkitManager()
-    
-    func saveNameItem(name: String,onSuccess:@escaping ()->Void){
-        let record = CKRecord(recordType: "MyItem")
-        
-        record.setValue(name, forKey: "Name")
-        
-        saveToDB(myrecord: record, dbType: .dbPublic){
-            onSuccess()
-        }
-    }
-    
-    private func saveToDB(myrecord:CKRecord,dbType: DBType,onSuccess:@escaping ()->Void){
-
-        switch dbType {
-        case .dbPublic:
-            dbPublic.save(myrecord) { record, error in
-                dump(record)
-                if record != nil,error == nil{
-                    print("saved")
-                    onSuccess()
-                }
-            }
-        case .dbPrivate:
-            dbPrivate.save(myrecord) { record, error in
-                if record != nil,error == nil{
-                    print("saved")
-                    onSuccess()
-                }
-            }
-        case .dbShared:
-            dbShared.save(myrecord) { record, error in
-                dump(record)
-                if record != nil,error == nil{
-                    print("saved")
-                    onSuccess()
-                }
-            }
-        }
-    }
-    
-    func fetchItem(recordType:String,onSuccess:@escaping ([Any])->Void){
-        let query = CKQuery(recordType: recordType, predicate: NSPredicate(value: true))
-        
-        dbPublic.perform(query, inZoneWith: nil) { records, error in
-            guard let records = records,error == nil else{
-                onSuccess([])
-                return
-            }
-            let items = records.compactMap{$0.value(forKey:"Name")}
-            
-            onSuccess(items)
-        }
-    }
-    
-}
 
 extension ViewController : UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
